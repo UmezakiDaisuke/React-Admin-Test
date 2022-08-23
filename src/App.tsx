48b9c2bffe6df6a33a63698aed6fbeb3c35acd69
UmezakiDaisuke <60737352+UmezakiDaisuke@users.noreply.github.com>
2022-08-23 13:55:01 +0900

    公式チュートリアル実践（Optimistic Rendering And Undoまで）

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
+import { PostCreate, PostEdit, PostList } from './posts';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+console.log(dataProvider);
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
 
 export default App;
---
+++ src/posts.tsx
@@ -0,0 +1,57 @@
+import {
+  Create,
+  Datagrid,
+  Edit,
+  EditButton,
+  List,
+  ReferenceField,
+  ReferenceInput,
+  SelectInput,
+  SimpleForm,
+  TextField,
+  TextInput,
+  useRecordContext,
+} from 'react-admin';
+
+export const PostList = () => (
+  <List>
+    <Datagrid>
+      <TextField source="id" />
+      <ReferenceField source="userId" reference="users">
+        <TextField source="name" />
+      </ReferenceField>
+      <TextField source="title" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+const PostTitle = () => {
+  const record = useRecordContext();
+  return <span>Post {record ? `${record.title}` : ''}</span>;
+};
+
+export const PostEdit = () => (
+  <Edit title={<PostTitle />}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PostCreate = (props: any) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <TextInput multiline source="body" />
+    </SimpleForm>
+  </Create>
+);
